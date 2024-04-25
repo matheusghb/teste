b384bc87c7fe3a5ef9089c3a0434e2136479f5c4
matheusghb <161875342+matheusghb@users.noreply.github.com>
2024-04-25 16:45:08 -0300

    Add files via upload

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
-from kivy.app import App # type: ignore
-from kivy.uix.button import Button # type: ignore
+from kivy.app import App
+from kivy.uix.button import Button
 class MyApp(App):
     def build(self):
-        return Button(text="Hello World! This is my first program in kivy \n I'm a SESIANO Student, and I love my Teacher")
+        return Button(text='Hello World! Eu estou programando com Kivy')
 if __name__ == '__main__':
     MyApp().run()
