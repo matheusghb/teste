c7b366859ef6c8846f5d14f45e68d1b10ddd2d17
PEDRO_IGORGAMESBR12 <55291388+KirbyBucciarati@users.noreply.github.com>
2024-04-29 17:43:50 -0300

    Add files via upload
    pedro igor

--- label.py
+++ label.py
@@ -3,16 +3,14 @@ from kivy.uix.label import Label
 from kivy.utils import get_color_from_hex
 
 class MinhaApp(App):
-    def build(self):
-        return Label(text="Olá SENAI!", 
-                     halign='left', 
-                     valign='top',
-                     size_hint=(None, None),
-                     size=(150,50),
-                     text_size=(150, None),
-                     font_size=10, 
-                     font_name="Arial", 
-                     color=get_color_from_hex('#FF5733'))
+    def build (self):
+        return Label(text='Olá, SENAI!', font_size= 100, font_name='Arial', color=get_color_from_hex('#FF5733'),
+        halign='left',
+        valign='top',
+        size_hint=(None, None),
+        size=(150,50),
+        text_size=(150, None))
+
     
 if __name__ == "__main__":
-    MinhaApp().run()
+ MinhaApp().run()
