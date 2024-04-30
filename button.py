07c8b5562b1d3d790fb7d9ee06e1362068b9f9da
PEDRO_IGORGAMESBR12 <55291388+KirbyBucciarati@users.noreply.github.com>
2024-04-30 17:33:26 -0300

    Add files via upload

--- button.py
+++ button.py
@@ -1,4 +1,29 @@
 from kivy.app import App 
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.utils import get_color_from_hex
+from kivy.uix.checkbox import CheckBox
+from kivy.uix.image import Image
 
-class minha
+
+class MinhaApp(App):
+    def build(self):
+     
+     layout = BoxLayout(orientation="vertical")
+
+     self.imag = Image(source='Users/aluno.sesipaulista/Downloads/praia.jpg')
+     self.chkb = CheckBox(active=False)
+     self.btn = Button(text='Clique Aqui')
+     self.btn.bind(on_press=self.botao_pressionado, font_size=50, background_color=get_color_from_hex('#3498db'), size_hint=(0.5, 0.2))
+
+     layout.add_widget(self.imag)
+     layout.add_widget(self.chkb)
+     layout.add_widget(self.btn)
+
+     return layout
+    
+    def botao_pressionado(self, instance):
+     print("Botão pressionado!")
+
+if __name__ == "__main__":
+ MinhaApp().run()
--- slider.py
+++ slider.py
@@ -1,19 +1,9 @@
 from kivy.app import App
 from kivy.uix.slider import Slider
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
 
 class MinhaApp(App):
     def build(self):
-        layout = BoxLayout(orientation="vertical", padding=20)
-        slider = Slider(min = 0, max = 100, value = 50, step = 1)
-        slider.bind(value=self.atualizar_label)
-        self.label = Label(text="Valor do slider: {}".format(int(slider.value)))
-        layout.add_widget(slider)
-        layout.add_widget(self.label)
-        return layout
-    def atualizar_label(self, instance, valor):
-        self.label.text = "valor do slider: {}".format(int(valor))
+        return Slider(min=0, max=100, value=50)
     
 if __name__ == "__main__":
-    MinhaApp().run
+ MinhaApp().run()
