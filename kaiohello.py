9f23415f33fea5cb9cc791173029d7ddb810ab39
kaiomarian <161875538+kaiomarian@users.noreply.github.com>
2024-05-07 16:35:53 -0300

    Update kaiohello.py

--- kaiohello.py
+++ kaiohello.py
@@ -3,14 +3,16 @@ from kivy.uix.label import Label
 from kivy.utils import get_color_from_hex 
 from kivy.uix.button import Button 
 from kivy.uix.boxlayout import BoxLayout 
-from kivy.uix.image import Image 
+from kivy.uix.image import Image, AsyncImage
 from kivy.uix.slider import Slider
 
+def btnpress(instance):
+    print('oioies')
 class app(App):
     def build(self):
         
-        layout = BoxLayout(orientation='vertical', padding=20)  # halign e valign nao funciona :3
-        self.lab1 = Label(
+        layout = BoxLayout(orientation='horizontal', padding=50, size = 50)  
+        self.oi = Label(
             text="oie",
             size=(100,200),
             font_size=100,
@@ -19,19 +21,26 @@ class app(App):
             valign='top',
             color= get_color_from_hex('FF0076'),
             italic= 'true')
-        self.lab2 = Button(
+        self.oi2 = ui = Button(
             size=(50,30),
             text="olis",
             font_size=50,
             font_name='arial',
-            color = [1,0,0,1],
+            color = get_color_from_hex('ffffff'),
+            background_color = get_color_from_hex('639E42'),
             halign='left',
             valign='top')
-
-        slider = Slider( min=0, max=100, value= 50, step=1) # imagem crasha o app >.<
+           
         
-        layout.add_widget(self.lab1)
-        layout.add_widget(self.lab2)
+        self.oi3 = Image( 
+            
+            source = "/Users/aluno.sesipaulista/Downloads/oi.jpg")
+
+        slider = Slider( min=0, max=100, value= 50, step=1) 
+        
+        layout.add_widget(self.oi)
+        layout.add_widget(self.oi2)
+        layout.add_widget(self.oi3)
         layout.add_widget(slider)
         return layout       
 
