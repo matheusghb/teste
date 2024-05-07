09097a2fed834160b98140c8c00bdd88501c50ae
kaiomarian <161875538+kaiomarian@users.noreply.github.com>
2024-05-07 17:01:52 -0300

    Update kaiohello.py

--- kaiohello.py
+++ kaiohello.py
@@ -3,40 +3,43 @@ from kivy.uix.label import Label
 from kivy.utils import get_color_from_hex 
 from kivy.uix.button import Button 
 from kivy.uix.boxlayout import BoxLayout 
-from kivy.uix.image import Image, AsyncImage
+from kivy.uix.image import Image
 from kivy.uix.slider import Slider
+from kivy.uix.floatlayout import FloatLayout
 
 def btnpress(instance):
     print('oioies')
 class app(App):
     def build(self):
         
-        layout = BoxLayout(orientation='horizontal', padding=50, size = 50)  
+        layout = FloatLayout()  
         self.oi = Label(
             text="oie",
-            size=(100,200),
+            size=(100,50),
+            size_hint = (None, None),
             font_size=100,
             font_name='arial',
-            halign='right',
-            valign='top',
+            pos_hint={'x': 0.6, 'y': 0.8},
             color= get_color_from_hex('FF0076'),
             italic= 'true')
         self.oi2 = ui = Button(
             size=(50,30),
+            size_hint = (None, None),
             text="olis",
             font_size=50,
             font_name='arial',
             color = get_color_from_hex('ffffff'),
             background_color = get_color_from_hex('639E42'),
-            halign='left',
-            valign='top')
+            pos_hint = {'center_x': 0.9, 'center_y': 0.5})
            
-        
         self.oi3 = Image( 
-            
-            source = "/Users/aluno.sesipaulista/Downloads/oi.jpg")
+            source = "/Users/aluno.sesipaulista/Downloads/oi.jpg",
+            size_hint = (None, None),
+            size=(50,30),
+            )
 
-        slider = Slider( min=0, max=100, value= 50, step=1) 
+        slider = Slider( size= (100, 50), size_hint = (None, None), min=0, max=100, value= 50, step=1,
+                        pos_hint={'x':0.1, 'right': 0.5}) 
         
         layout.add_widget(self.oi)
         layout.add_widget(self.oi2)
