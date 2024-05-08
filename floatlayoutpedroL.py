c10e9773a90e6b97ced40d981d39c17e43f1d6cd
PedroLucaspllgc <161877989+PedroLucaspllgc@users.noreply.github.com>
2024-05-08 13:54:15 -0300

    Create floatlayoutpedroL.py

---
+++ floatlayoutpedroL.py
@@ -0,0 +1,7 @@
+from kivy.uix.button import Button
+from kivy.uix.floatlayout import Floatlayout
+
+layout_float = FloatLayout()
+
+botao = Button(text='Clique Aqui', size_hint=(None, None), size=(100, 50), pos_hint={'x': 0.5, 'y': 0.5})
+layout_float.add_widget(botao)
